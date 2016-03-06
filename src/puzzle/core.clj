54e5cb4ab62afc634ea98b7da55557dd7d6d4836
Hideto Ishibashi <hidetoi@gmail.com>
2016-03-06 23:38:30 +0900

    Translate `puzzle.komachi` to ClojureScript (and add Devcards)

--- src/puzzle/core.clj
+++
@@ -1,8 +0,0 @@
-(ns puzzle.core
-  (:gen-class)
-  (:require [puzzle.komachi :as komachi]))
-
-(defn -main
-  "Display a usage"
-  [& args]
-  (println "Komachi Mushi Kui Zan (小町虫食い算): $ lein run -m puzzle.komachi"))
